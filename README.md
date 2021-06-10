ceb1a80b30b8dfe38f050c45ed9b1295e250995a
wlawler45 <williamgmlawler@yahoo.com>
2021-06-10 17:17:11 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# oarbot
+OARbot_Control
+
---
+++ script/ROS_UWB_LEC_back.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+import serial
+#import time
+#from termcolor import colored
+
+## Just for Windows
+#import os
+#os.system('color')
+try:
+
+	ser = serial.Serial('/dev/ttyACM3', 115200, timeout =2, xonxoff=True)
+
+
+	pub = rospy.Publisher('oarbot/uwb_serial_back', String, queue_size=10)
+	rospy.init_node('uwb_serial_node', anonymous=True, disable_signals=True)
+
+
+
+	# Two enter presses puts us into terminal mode
+	ser.write('\r')
+	ser.write('\r')
+	
+
+	# Wait until all the startup stuff is done
+	for i in range(15):
+		ser_bytes = ser.readline()
+		print(ser_bytes)
+		if "dwm> " in ser_bytes:
+			break
+
+	# Tell UWB tag to give us distance readings
+
+	if not "DIST" in ser_bytes:
+		ser.write("lec\r")
+	ser_bytes = ser.readline() 
+	print(ser_bytes)
+
+	# Throw out first reading (has extra "dwm> "")
+	ser_bytes = ser.readline() 
+	print(ser_bytes)
+
+	while True:
+		ser_bytes = ser.readline()
+		if(ser_bytes):
+			rospy.loginfo(ser_bytes)
+			pub.publish(ser_bytes)
+
+except KeyboardInterrupt:
+	print('Interrupt!')
+
+	# Turn off lec
+	ser.write('\r')
+	# This turns the terminal mode off
+	ser.write('quit\r')
+	ser_bytes = ser.readline() 
+	print(ser_bytes)
+	ser_bytes = ser.readline() 
+	print(ser_bytes)
+	ser_bytes = ser.readline() 
+	print(ser_bytes)
+	ser_bytes = ser.readline() 
+	print(ser_bytes)
+
+	ser.close()
